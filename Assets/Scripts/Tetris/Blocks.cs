bd0286302cd1bceec87929a70768dc335b4bc87a
kai <kaiion0509@gmail.com>
2020-12-06 01:27:15 -0500

    created seven blocks and started on block movement
    added shapes image
    
    creating block shapes
    added 7 block shapes - S, O, T, L, J, Z, I
    
    added block method to each shape prefab, invoke block falling once game starts
    Set square can rotate var to false
    
    Working on game controller next

---
+++ Assets/Scripts/Tetris/Blocks.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+    public bool canRotate = true;   //check if block can rotate
+
+    //movemethod
+    void Move(Vector3 moveDirection)
+    {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void RotateLeft()
+    {
+        if (canRotate)                  // only rotate if it can 
+        {
+            transform.Rotate(0, 0 , -90);
+        }
+    }
+
+    public void RotateRight()
+    {
+        if (canRotate)                  // only rotate if it can 
+        {
+            transform.Rotate(0, 0 , 90);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MoveDown", 0, 0.5f);       //calls down method and make block fall every .5 sec        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
