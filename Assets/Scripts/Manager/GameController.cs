72b30b070e53b29ef4081524c790840e6a8628e7
kai <kaiion0509@gmail.com>
2020-12-06 10:29:50 -0500

    added spawner and controll script
    Add Spawner script and Controll script
    
    added tags to call the two scripts
    added code to reference them
    added random spawner method
    set default spawn location to (4,25,-1)
    
    added vectorf helper script
    used it to round block coordinates so it wont be at odd spots

---
+++ Assets/Scripts/Manager/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    Layout layout;      //reference to layout script
+
+    [SerializeField]
+    Spawner spawner;    //refernce spawner script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //add references to layout and spawner component
+        layout = GameObject.FindWithTag("Layout").GetComponent<Layout>();
+        spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+        /**
+        FindObjectOfType<s>, same function as above but slower
+        **/
+        //layout = GameObject.FindObjectOfType<Layout>();   //another find script method
+        //spawner = GameObject.FindObjectOfType<Spawner>();   //another find script method
+
+        /**
+        incase for null elements
+        **/
+        if (spawner)
+        {
+            spawner.transform.position = Vectorf.Round(spawner.transform.position);
+        }
+
+        if(!layout)
+        {
+            Debug.LogWarning("No Layout Defined!");
+        }
+
+        if(!spawner)
+        {
+            Debug.LogWarning("No Spawner Defined!");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tetris/Spawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    public Blocks[] blocks;       //make array element for the 7 blocks
+
+    /**
+        Method to get random block
+    **/
+    Blocks GetRandomeBlock()
+    {
+        int i = Random.Range(0, blocks.Length); //get random int range to num of blocks
+        
+        /*
+            Check null case
+        */
+        if (blocks[i])
+        {
+            return blocks[i];
+        }
+        else
+        {
+            Debug.LogWarning("Invalid!");
+            return null;
+        }
+    }
+
+    /*
+        Shape Spawn method
+    */
+    public Blocks SpawnBlock()
+    {
+        Blocks block = null;
+        block = Instantiate(GetRandomeBlock(), transform.position, Quaternion.identity) as Blocks;
+
+        /*
+        null case
+        */
+        if (block)
+        {
+            return block;
+        }
+        else
+        {
+            Debug.LogWarning("Invalid!");
+            return null;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //call helper script to make sure block be on whole number location
+        Vector2 originalVector = new Vector2(4.3f, 1.3f);
+        Vector2 newVector = Vectorf.Round(originalVector);
+
+        Debug.Log(newVector.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
